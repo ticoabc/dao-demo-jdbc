b1dde8c36db20f0fc1f9ff1a6405ae138adc1ab5
Tiago de Freitas <tiagoabc@gmail.com>
2023-08-17 16:58:45 -0300

    Create findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -104,5 +107,54 @@ public class SellerDaoJDBC implements SellerDao{
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			
+			//Controle para não criar objetos 'Department e sim aponta para um só
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//Percorre enquanto houver departamentos
+			while(rs.next()) {
+				
+				//Testa e (ou) procura se o Department ja existe
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				//Se DepartmentID for null ele instacia 
+				if(dep == null) {
+					//Chamada por Métoto Auxiliar (mais enxuto)
+					dep = instantiateDepartment(rs);					
+					//Salva e reaproveita
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				//Chamada por Métoto Auxiliar (mais enxuto)
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 }
